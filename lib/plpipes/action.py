9e5735d1062e1f231ff1798f5e7ad4a8693d6a46
Salvador Fandiño <salvador.fandino@predictland.com>
2022-12-16 18:17:37 +0100

    WIP: actions

--- lib/plpipes/action.py
+++ lib/plpipes/action.py
@@ -1,10 +1,13 @@
-import cfg
 import pathlib
-import plpipes.config
+import logging
+import time
+from plpipes import cfg
+
 _cache = {}
 
-suffix_aliases = { 'yaml': ['yml'] }
-suffixes = ['yaml', 'json', 'table.sql', 'py']
+suffix_aliases = { 'yaml': ['yml'],
+                   'table_sql': ['table.sql'] }
+suffixes = ['yaml', 'json', 'table_sql', 'py']
 
 def _find_action_files(fn_start):
     files = {}
@@ -12,7 +15,10 @@ def _find_action_files(fn_start):
         files["dir"] = str(fn_start)
     for suffix in suffixes:
         for alias in (suffix, *suffix_aliases.get(suffix, [])):
-            fn = fn_start.with_suffix(alias)
+
+            print(f"fn_start: {fn_start}, suffix: {suffix}")
+
+            fn = fn_start.with_suffix("."+alias)
             if fn.exists():
                 files[suffix] = str(fn)
                 break
@@ -21,49 +27,50 @@ def _find_action_files(fn_start):
 def lookup(name):
 
     if name not in _cache:
-        actions_dir = pathlib.Path(cfg.fs.actions)
+        actions_dir = pathlib.Path(cfg["fs.actions"])
         files = _find_action_files((actions_dir / name.replace(".", "/")).absolute())
-        acfg_prefix = "actions." + ".children.".join(name.split("."))
-        acfg_low = { 'files': files }
-        if "py" in files:
-            acfg["type"] = "python"
-        elif "sql" in files:
-            acfg["type"] = "sql"
 
-        acfg = plpipes.config.ConfigStack()
-        acfg.push(acfg_low, prefix=acfg_prefix)
-        acfg.push(cfg._to_tree())
-        except KeyError: pass
+        path = "actions." + ".children.".join(name.split("."))
+        acfg = cfg.cd(path)
+
         for ext in ("yaml", "json"):
             if ext in files:
-                acfg.push_file(files[ext], prefix=acfg_prefix)
-        try: acfg.push(cfg._to_tree)
-        except KeyError: pass
+                acfg.merge_file(files[ext], frame=-1)
+
+        for k, v in files.items():
+            acfg.setdefault(f"files.{k}", v)
+
+        if "py" in files:
+            acfg.setdefault("type", "python_script")
+        elif "table_sql" in files:
+            acfg.setdefault("type", "sql_table_creator")
 
         try:
-            acfg_branch = acfg._browse(acfg_prefix)
-            action_type = acfg_branch.type
-        except IndexError:
+            action_type = acfg["type"]
+        except KeyError:
             raise ValueError(f"Action {name} has no type declared or action file not found")
 
         action_class = _class_lookup(action_type)
 
-        _cache[name] = action_class(name, acfg_branch)
+        _cache[name] = action_class(name, acfg)
 
     return _cache[name]
+
+def run(name):
+    lookup(name).run()
 
 _class_registry = {}
 
 def _class_lookup(type):
     if type in _class_registry:
-        return _class_registry(type)
+        return _class_registry[type]
     raise ValueError(f"Unsupported action type {type}")
 
 def register_class(type, action_class):
     _class_registry[type] = action_class
 
 class Action:
-        def __init__(self, name, action_cfg):
+    def __init__(self, name, action_cfg):
         self._name = name
         self._cfg = action_cfg
 
@@ -91,12 +98,14 @@ class _SqlTableCreator(Action):
 
     def do_it(self):
 
-        path = pathlib.Path(self._cfg.files.sql)
+        path = pathlib.Path(self._cfg["files.table_sql"])
         with open(path, "r") as f:
             sql = f.read()
-        table_name = path.stem
+        table_name = path.stem.split(".")[0]
         from plpipes.database import create_table
         create_table(table_name, sql)
+
+register_class("sql_table_creator", _SqlTableCreator)
 
 
 class _PythonRunner(Action):
@@ -112,7 +121,7 @@ class _PythonRunner(Action):
 
         if self._new_action_class is None:
 
-            path = pathlib.Path(self._cfg.files.py)
+            path = pathlib.Path(self._cfg["files.py"])
             with open(path, "r") as f:
                 code = f.read()
             exec(code, {}, {'replace_action': self.replace_action})
@@ -123,3 +132,5 @@ class _PythonRunner(Action):
             self._new_action = self._new_action_class(self._name, self._cfg)
 
         self._new_action._do_it(indent)
+
+register_class("python_script", _PythonRunner)
