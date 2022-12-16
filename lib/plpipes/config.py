40638a45f8dd425b840be9c6164604183be8775c
Salvador Fandiño <salvador.fandino@predictland.com>
2022-12-16 18:16:42 +0100

    Redesing config module

--- lib/plpipes/config.py
+++ lib/plpipes/config.py
@@ -2,184 +2,203 @@
 import yaml
 import logging
 import sys
-import collections.abc
 import re
+import copy
+import collections.abc
 
 from pathlib import Path
 
-_NO_DEFAULT={"-no-default": True}
-
 class ConfigStack:
+    def __init__(self, top=None):
+        if not top:
+            top = _Level()
+        self._top = top
 
-    def __init__(self):
-        self._stack = []
+    def _cd(self, path):
+        return _Ptr(self, path)
 
     def root(self):
-        return _ConfigMapping(self, [])
+        return self._cd("")
 
-    def _path_to_string(self, path, ix=None):
-        if ix is not None:
-            path = path[:ix]
-        return ".".join(path)
+    def reset_cache(self):
+        self._top._reset_cache()
 
-    def _wrap(self, path, val):
-        if isinstance(val, list):
-            return _ConfigCollection(self, path)
-        if isinstance(val, dict):
-            return _ConfigMapping(self, path)
-        return val
+class _Ptr(collections.abc.MutableMapping):
+    def __init__(self, stack, path):
+        self._path = path
+        self._stack = stack
 
-    def _get(self, path, default=_NO_DEFAULT, _has=False):
-        anchored = False # Once we go into an array or a non-dict
-                         # object we stop the stack lookup-merge
-                         # process. This flag indicates that
-                         # condition.
-        for level in self._stack:
-            try:
-                p = level
-                for ix, key in enumerate(path):
-                    if isinstance(p, list):
-                        anchored = True
-                        p = p[key]
-                    elif isinstance(p, dict):
-                        p = p[key]
-                    else:
-                        raise KeyError(self._path_to_string(path, ix))
-                return True if _has else self._wrap(path, p)
-            except (IndexError, KeyError):
-                pass
-            if anchored:
-                break
-        if default is _NO_DEFAULT:
-            raise KeyError(self._path_to_string(path))
-        return default
+    def cd(self, key):
+        return self._stack._cd(self._mkkey(key)) if key else self
 
-    def _has(self, path):
-        return self._get(path, _has=True)
+    def _mkkey(self, key):
+        if key:
+            if self._path:
+                return f"{self._path}.{key}"
+            return key
+        return self._path
 
-    def _dict_iterator(self, path):
-        anchored = False
-        keys = set()
-        for level in self._stack:
-            try:
-                p = level
-                for key in path:
-                    p = p[key]
-                    if not isinstance(p, dict):
-                        anchored = True
+    def __getitem__(self, key):
+        return self._stack._top._get(self._mkkey(key))
 
-                if isinstance(p, dict):
-                    for k in p:
-                        if k not in keys:
-                            yield k
-                            keys.add(k)
+    def __contains__(self, item):
+        return self._stack._top._contains(self._mkkey(key))
 
-            except (IndexError, KeyError):
-                pass
-            if anchored:
-                break
-        return
+    def __setitem__(self, key, value):
+        return self._stack._top._set(self._mkkey(key), value)
 
-    def _list_iterator(self, path):
-        anchored = False
-        for level in self._stack:
-            try:
-                p = level
-                for key in path:
-                    p = p[key]
-                    if not isinstance(p, dict):
-                        anchored = True
+    def __delitem__(self, key):
+        self._stack._level._del(key)
 
-                assert isinstance(p, list)
-                for ix, v in enumerate(p):
-                    yield self._wrap(path + [ix], v)
-                return
-            except (IndexError, KeyError):
-                pass
-            if anchored:
-                break
-        return
+    def __len__(self):
+        self._stack._level._len()
 
-    def squash(self):
-        pass
+    def to_tree(self, key=""):
+        return self._stack._top._to_tree(self._mkkey(key))
 
-    def push(self, level, prefix=None):
-        if prefix is not None and prefix != "":
-            path = prefix.split(".")
-            new_level = {}
-            r = new_level
-            for p in path[:-1]:
-                r = r.setdefault(p, {})
-            r[path[-1]] = level
-            level = new_level
-        assert isinstance(level, dict)
-        self._stack.insert(0, level)
+    def merge(self, tree, key="", frame=0):
+        return self._stack._top._merge(self._mkkey(key), tree, frame)
 
-    def push_file(self, fn, prefix=None):
+    def merge_file(self, fn, path="", frame=0):
         logging.debug(f"Reading configuration file {fn}")
         with open(fn, "r+", encoding="utf8") as f:
             if re.search(r'\.ya?ml', fn, re.IGNORECASE):
                 import yaml
-                level = yaml.safe_load(f)
+                tree = yaml.safe_load(f)
             elif re.search(r'\.json', fn, re.IGNORECASE):
                 import json
-                level = json.load(f)
-        self.push(level, prefix)
+                tree = json.load(f)
+        self.merge(self._mkkey(key), tree, path, frame)
 
-    def pop(self, level):
-        self._stack.pop()
+    def fs(self, key, set_default=None):
+        if default:
+            return Path(self[f"fs.{key}"])
 
-class _ConfigPtr:
-    def __init__(self, stack, path):
-        super().__init__()
-        self._stack=stack
-        self._path=path
+    def squash_frames(self):
+        self._stack._top._squash_frames()
 
-    def __getattr__(self, key):
-        return self._stack._get(self._path + [key])
-
-    def __getitem__(self, key):
-        return self._stack._get(self._path + [key])
-
-    def __setitem__(self, key, value):
-        return self._stack._set(self._path + [key], value)
-
-    def __contains__(self, key):
-        return self._stack._contains(self._path + [key])
-
-    def __len__(self):
-        len = 0
-        for _ in self:
-            len += 1
-        return len
-
-    def _child_as_tree(self, child):
-        if isinstance(child, _ConfigPtr):
-            return child._to_tree()
-        return child
-
-    def __repr__(self):
-        return repr(self._to_tree())
+    def __iter__(self):
+        ...
 
     def __str__(self):
-        return str(self._to_tree())
+        return str(self._stack._top._to_tree(""))
 
-    def _browse(self, path):
-        return self._stack._get(self._path + path.split("."))
+def _merge_any(tree, new):
+    if isinstance(new, dict):
+        if isinstance(tree, dict):
+            for k, v in new.items():
+                tree[k] = _merge_any(tree[k], v) if k in tree else copy.deepcopy(v)
+            return tree
+    return copy.deepcopy(new)
 
-class _ConfigMapping(_ConfigPtr, collections.abc.Mapping):
+class _Level:
+    def __init__(self, root={}, parent=None):
+        self._root = {}
+        self._parent = parent
+        self._merge("", root)
 
-    def _to_tree(self):
-        return { k: self._child_as_tree(self[k]) for k in self }
+    def _reset_cache(self):
+        self._cache={}
+        if self._parent:
+            self._parent._reset_cache(self)
 
-    def __iter__(self):
-        return self._stack._dict_iterator(self._path)
+    def _merge(self, key, value, frame=0):
+        if frame < 0:
+            if not self._parent:
+                self._parent = _Level()
+            self._parent._merge(key, value, frame+1)
+        else:
+            tree = self._root
+            if key:
+                parts = key.split(".")
+                last = parts.pop()
+                for p in parts:
+                    if (p not in tree) or (not isinstance(tree[p], dict)):
+                        tree[p] = {}
+                    tree = tree[p]
+                tree[last] = _merge_any(tree.get(last, None), value)
+            else:
+                if not isinstance(value, dict):
+                    raise ValueError("Top configuration must be a dictionary")
+                self._root = _merge_any(tree, value)
+        self._cache = {}
 
-class _ConfigCollection(_ConfigPtr, collections.abc.Collection):
+    def _merge_default(self, key, value):
+        ...
 
-    def _to_tree(self):
-        return [ self._child_as_tree(v) for v in self ]
+    def _set(self, key, value):
+        if isinstance(value, dict) or isinstance(value, list):
+            raise ValueError("It is not possible to set a configuration entry to a dictionary or list, use merge instead")
+        elif not isinstance(value, int):
+            value = str(value)
+        self._merge(key, value)
 
-    def __iter__(self):
-        return self._stack._list_iterator(self._path)
+    def _get_nocache(self, key):
+        parts = key.split(".")
+        v = self._root
+        for p in parts:
+            try:
+                v = v[p]
+            except TypeError:
+                raise KeyError(f"Config key '{key}' traversing blocked by a non dictionary object")
+            except KeyError as ex:
+                if self._parent:
+                    return self._parent._get(key)
+                raise ex
 
+        if isinstance(v, dict) or isinstance(v, list):
+            raise KeyError(f"config key '{key}' does not point to a terminal node")
+        return v
+
+    def _get(self, key):
+        if key not in self._cache:
+            self._cache[key] = self._get_nocache(key)
+        return self._cache[key]
+
+    def _contains(self, key):
+        if key in self._cache:
+            return True
+        parts = key.split(".")
+        v = self._root
+        for p in parts:
+            try:
+                v = v[p]
+            except TypeError:
+                return False
+            except KeyError as ex:
+                return self._parent._contains(self, key) if self._parent else False
+        return True
+
+    def _to_tree(self, key):
+        parent_ex = None
+        tree = {}
+        if self._parent:
+            try:
+                tree = self._parent._to_tree(key)
+            except Exception as ex:
+                parent_ex = ex
+
+        v = self._root
+        if key:
+            parts = key.split(".")
+            for p in parts:
+                try:
+                    v = v[p]
+                except TypeError:
+                    raise KeyError(f"Config key '{key}' traversing blocked by a non dictionary object")
+                except:
+                    if parent_ex:
+                        raise parent_ex
+                    return tree
+        return _merge_any(tree, v)
+
+    def _steal_root(self):
+        self._squash_frames()
+        root = self._root
+        del self._root
+        return root
+
+    def _squash_frames(self):
+        if self._parent:
+            self._tree = _merge_any(self._parent._steal_root(), self._root)
+            self._parent = None
