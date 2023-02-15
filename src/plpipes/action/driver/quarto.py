22ad1111cea1d1334656145df439fd719c0a2278
Salvador Fandiño <salvador.fandino@predictland.com>
2023-02-15 18:59:38 +0100

    WIP: Quarto driver

--- src/plpipes/action/driver/quarto.py
+++ src/plpipes/action/driver/quarto.py
@@ -7,8 +7,9 @@ from pathlib import Path
 import yaml
 from contextlib import contextmanager
 import os
-import shutil
+import tempfile
 import json
+import shutil
 
 def _read_yaml_header(fn):
     with open(fn, "r") as f:
@@ -20,7 +21,7 @@ def _read_yaml_header(fn):
             if in_yaml:
                 if line == "---":
                     logging.debug(f"YAML header read from Quarto file at {fn}")
-                    return "\n".join(*yaml, "")
+                    return "\n".join([*yaml, ""])
                 else:
                     yaml.append(line)
             else:
@@ -54,8 +55,10 @@ plpipes.action.driver.quarto._init_plpipes(""" + repr(str(config.absolute())) + 
                             state = "copy"
                     elif state == "in_yaml":
                         if stripped == "---":
-                            dest_f.write(patch)
-                            state = "copy"
+                            state = "after_yaml"
+                    elif state == "after_yaml":
+                        dest_f.write(patch)
+                        state = "copy"
                     else:
                         raise Exception("Internal error, unexpected state")
                 dest_f.write(line)
@@ -76,45 +79,60 @@ def _cd(newdir):
 class _QuartoRunner(Action):
 
     def __init__(self, name, action_cfg):
-        self._path = Path(action_cfg["files.qmd"])
+        self._path = Path(action_cfg["files.qmd"]).absolute()
         header = _read_yaml_header(self._path)
         if header is not None:
-            action_cfg.merge(yaml.load(header), key="quarto")
-            action_cfg.copydefaults(action_cfg.cd("quarto.plpipes"),
-                                    ["dest.dir", "dest.key", "dest.file", "dest.format"])
+            action_cfg.merge(yaml.load(header, Loader=yaml.SafeLoader), key="quarto")
+        dcfg = action_cfg.cd("dest")
+        scfg = action_cfg.cd("quarto.plpipes.dest")
+        dcfg.copydefaults(scfg,
+                          key='work',
+                          dir='',
+                          format="html")
+        default_file = self._path.with_suffix("." + dcfg["format"]).name
+        dcfg.copydefaults(scfg, file=default_file)
         super().__init__(name, action_cfg)
 
     def do_it(self):
-        qmd_path = Path(self._path).absolute()
-        dest_key = self._cfg.setdefault("dest.key", "work")
-        dest_dir = self._cfg.setdefault("dest.dir", "")
-        dest_format = self._cfg.setdefault("dest.format", "html")
-        dest_file = self._cfg.setdefault("dest.file", str(qmd_path.with_suffix("." + dest_format).name))
+        logging.debug(f"Action config: {self._cfg.to_tree()}")
+        key_dir = Path(cfg["fs." + self._cfg['dest.key']])
+        target_path = key_dir / self._cfg['dest.dir'] / self._cfg['dest.file']
+        stem = target_path.stem
 
-        dest_file_path = Path(cfg[f"fs.{dest_key}"]) / dest_dir / dest_file
-        dest_dir_path = dest_file_path.parent
-        dest_dir_path.mkdir(parents=True, exist_ok=True)
-
-        env = os.environ.copy()
-        env['PLPIPES_ROOT_DIR'] = str(Path(cfg["fs.root"]).absolute())
-
-        with _cd(dest_dir_path):
-            dest_work_path = Path("quarto")
-            dest_work_path.mkdir(parents=True, exist_ok=True)
-            qmd_copy_path = dest_work_path / qmd_path.name
-            dest_cfg_path = (dest_work_path / qmd_path.name).with_suffix(".json")
-
-            # Dump the configuration into a temporary file
-            with open(dest_cfg_path, "w") as f:
+        with tempfile.TemporaryDirectory() as workdir:
+            workdir = Path(workdir)
+            temp_target_path = workdir / target_path.name
+            temp_cfg_path = workdir / "config.json"
+            with open(temp_cfg_path, "w") as f:
                 json.dump(cfg.to_tree(), f)
 
-            _patch_qmd(qmd_path, dest=qmd_copy_path, config=dest_cfg_path)
-            #shutil.copyfile(qmd_path, qmd_copy_path)
+            temp_qmd_path = workdir / f"{stem}.qmd"
+            _patch_qmd(self._path, temp_qmd_path, temp_cfg_path)
 
-            subprocess.run(["quarto", "render", qmd_copy_path,
-                            "--output", dest_file_path.name,
-                            "--to", dest_format,
-                            "--no-execute-daemon"],
-                           env=env)
+            env = os.environ.copy()
+            env['PLPIPES_ROOT_DIR'] = str(Path(cfg["fs.root"]).absolute())
+
+            with _cd(workdir):
+                cmd = ["quarto", "render", temp_qmd_path.name,
+                       "--output", temp_target_path.name,
+                       "--to", self._cfg["dest.format"],
+                       "--no-execute-daemon"]
+
+                logging.debug(f"Running quarto: {cmd}, cwd: {os.getcwd()}")
+                proc = subprocess.run(cmd, env=env)
+                if proc.returncode != 0:
+                    raise Exception("Quarto command failed, RC: {proc.returncode}")
+
+                logging.debug("Moving Quarto output files to final destination")
+                target_path.parent.mkdir(parents=True, exist_ok=True)
+                shutil.copy2(temp_target_path,
+                             target_path)
+                aux_dir_path = Path(f"{stem}_files")
+                if aux_dir_path.is_dir():
+                    shutil.copytree(aux_dir_path,
+                                    target_path.parent / aux_dir_path,
+                                    dirs_exist_ok=True)
+
+        logging.info(f"Quarto document available at {target_path}")
 
 register_class("quarto", _QuartoRunner, "qmd")
