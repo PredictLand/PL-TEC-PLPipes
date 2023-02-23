b585e9e4fc25b09d37695fe2a669912b28bcecb0
Salvador Fandiño <salvador.fandino@predictland.com>
2023-02-23 10:00:43 +0100

    add database drivers for SQLServer and AzureSQL

--- src/plpipes/database.py
+++ src/plpipes/database.py
@@ -225,11 +225,33 @@ group by {', '.join(by)}
 
 class _ODBCDriver(_Driver):
     def __init__(self, name, drv_cfg):
-        connection_string = f"driver={drv_cfg['odbc_driver']};Server={drv_cfg['server']};Database={drv_cfg['database']};UID={drv_cfg['user']};PWD={drv_cfg['pwd']}"
-        url = sqlalchemy.engine.URL.create("mssql+pyodbc",
-                                           query={"odbc_connect": connection_string})
-
+        url = sqlalchemy.engine.URL.create(drv_cfg['sql_alchemy_driver'],
+                                           query={'odbc_connect': drv_cfg['connection_string']})
         super().__init__(name, drv_cfg, url)
+
+class _SQLServerDriver(_ODBCDriver):
+    def __init__(self, name, drv_cfg):
+        if "connection_string" not in drv_cfg:
+            driver = drv_cfg.setdefault('driver_odbc', '{ODBC Driver 18 for SQL Server}')
+            port = drv_cfg.setdefault('port', 1433)
+            low_level_proto = drv_cfg.setdefault('low_level_proto', 'tcp')
+            encrypt = 'yes' if drv_cfg.setdefault('encrypt', True) else 'no'
+            trusted_server_certificate = 'yes' if drv_cfg.setdefault('trusted_server_sertificate', True) else 'no'
+            timeout = drv_cfg.setdefault('timeout', 60)
+            server = drv_cfg['server']
+            database = drv_cfg['database']
+            uid = drv_cfg['user']
+            password = drv_cfg['password']
+
+            cs_log = f"Driver={driver};Server={low_level_proto}:{server},{port};Database={database};Uid={uid};Pwd=**********;Encrypt={encrypt};TrustedServerCertificate={trusted_server_certificate};Connection Timeout={timeout}"
+            logging.debug(f"ODBC connection string for {name} is {cs_log}")
+            cs     = f"Driver={driver};Server={low_level_proto}:{server},{port};Database={database};Uid={uid};Pwd={password};Encrypt={encrypt};TrustedServerCertificate={trusted_server_certificate};Connection Timeout={timeout}"
+            drv_cfg.setdefault('connection_string', cs)
+        drv_cfg.setdefault('sql_alchemy_driver', 'mssql+pyodbc')
+        super().__init__(name, drv_cfg)
+
+class _AzureSQLDriver(_SQLServerDriver):
+    pass
 
 class _DuckDBDriver(_LocalFileDriver):
     def __init__(self, name, drv_cfg):
@@ -250,6 +272,8 @@ class _DuckDBDriver(_LocalFileDriver):
 _driver_class["duckdb"] = _DuckDBDriver
 _driver_class["sqlite"] = _SQLiteDriver
 _driver_class["odbc"]   = _ODBCDriver
+_driver_class["sql_server"] = _SQLServerDriver
+_driver_class["azure_sql"] = _AzureSQLDriver
 
 def query(sql, db=None, **parameters):
     return lookup(db).query(sql, parameters)
