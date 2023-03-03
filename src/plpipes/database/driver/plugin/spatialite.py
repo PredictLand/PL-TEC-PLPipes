e937563c1802c74896ac27d93e456c918419ea03
Salvador Fandiño <salvador.fandino@predictland.com>
2023-03-03 16:26:22 +0100

    add support for spatialite database

---
+++ src/plpipes/database/driver/plugin/spatialite.py
@@ -0,0 +1,20 @@
+import logging
+
+from plpipes.database.driver.sqlite import SQLiteDriver
+from plpipes.plugin import plugin
+
+from sqlalchemy import event
+
+@plugin("spatialite")
+class SpatialiteDriver(SQLiteDriver):
+
+    def __init__(self, name, drv_cfg):
+        super().__init__(name, drv_cfg)
+
+        @event.listens_for(self._engine, "connect")
+        def init_spatialite_connection(conn, cr):
+            logging.debug("Initializing SQLite connection, loading mod_spatialite")
+            conn.enable_load_extension(True)
+            conn.execute("select load_extension('mod_spatialite')")
+
+        self.execute("select InitSpatialMetaData()", None)
