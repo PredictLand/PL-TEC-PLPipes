641f62ca405a856ae48df2a56db8c7bb2fa7ba02
Salvador Fandiño <salvador.fandino@predictland.com>
2023-03-26 22:13:45 +0200

    Add support for action of type archive_unpacker

--- src/plpipes/action/__init__.py
+++ src/plpipes/action/__init__.py
@@ -4,6 +4,7 @@ from .driver import sql
 from .driver import downloader
 from .driver import quarto
 from .driver import file_downloader
+from .driver import archive_unpacker
 
 # import the runner
 from .runner import run
