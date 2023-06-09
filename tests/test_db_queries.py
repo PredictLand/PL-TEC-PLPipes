38dd26ecef30b3e509334aef4f6ad96c737f4a3e
Oscar Brizuela <oscar.brizuela@predictland.com>
2023-06-09 15:32:37 +0200

    TEST QUERY CHUNKED

--- tests/test_db_queries.py
+++ tests/test_db_queries.py
@@ -1,6 +1,7 @@
 import tempfile
 import shutil
 import pandas
+import numpy
 import pytest
 import os
 import sys
@@ -39,17 +40,6 @@ def handle_db():
 def db_context():
     # Create a temporary directory
     with handle_db() as work_dir:
-        
-        file = open('myfile.txt', 'w')
-        original_stdout = sys.stdout
-        
-        sys.stdout = file
-
-        test_update_table_strictly_ascending(db_context)
-        # test_query_chunked(db_context)
-        # test_query_group(db_context)
-        # test_begin(db_context)
-
         yield work_dir
 
 # CREATE TABLE and QUERY test
@@ -139,62 +129,100 @@ def test_execute_drop(db_context):
 def test_update_table_strictly_ascending(db_context):
     df_dest = pandas.DataFrame({'a': [1, 3], 'b':[2, 5], 'c':[3, 5]})
     df_source = pandas.DataFrame({'a': [1, 3, 5], 'b': [2, 5, 7], 'c': [3, 5, 8]})
+    df_expected = df_source
     source_table_name = "test_table_1"
     dest_table_name = "test_table_2"
     db.create_table(source_table_name, df_source)
     db.create_table(dest_table_name, df_dest)
     db.update_table(source_table_name, dest_table_name, key='a', key_dir=">")
-    query_result_dest = db.query("select * from test_table_2")
-    print(query_result_dest)
+    query_result = db.query("select * from test_table_2")
+    assert df_expected.equals(query_result)
 
 # UPDATE TABLE ASCENDING test
 def test_update_table_ascending(db_context):
-    df_dest = pandas.DataFrame({'a': [5, 8], 'b':[None, None], 'c':[None, None]})
-    df_source = pandas.DataFrame({'a': [1, 2, 4, 6], 'b': [2, 5, 8, 9], 'c': [3, 6, 7, 9]})
+    df_dest = pandas.DataFrame({'a': [1, 2], 'b':[2, 5], 'c':[3, 3]})
+    df_source = pandas.DataFrame({'a': [1, 2, 2, 4], 'b': [2, 5, 6, 6], 'c': [3, 3, 5, 7]})
+    df_expected = df_source
     source_table_name = "test_table_1"
     dest_table_name = "test_table_2"
     db.create_table(source_table_name, df_source)
     db.create_table(dest_table_name, df_dest)
-    db.update_table(source_table_name, dest_table_name=source_table_name, key="a", key_dir=">=")
+    db.update_table(source_table_name, dest_table_name, key="a", key_dir=">=")
     query_result = db.query("select * from test_table_2")
-    print(query_result)
+    assert df_expected.equals(query_result)
 
 # UPDATE TABLE DESCENDING test
 def test_update_table_descending(db_context):
-    df_dest = pandas.DataFrame({'a': [5, 8], 'b':[None, None], 'c':[None, None]})
-    df_source = pandas.DataFrame({'a': [1, 2, 4, 6], 'b': [2, 5, 8, 9], 'c': [3, 6, 7, 9]})
+    df_dest = pandas.DataFrame({'a': [5, 4], 'b':[7, 6], 'c':[7, 5]})
+    df_source = pandas.DataFrame({'a': [5, 4, 4, 3], 'b': [7, 6, 6, 2], 'c': [7, 5, 3, 3]})
+    df_expected = df_source
     source_table_name = "test_table_1"
     dest_table_name = "test_table_2"
     db.create_table(source_table_name, df_source)
     db.create_table(dest_table_name, df_dest)
-    db.update_table(source_table_name, dest_table_name=source_table_name, key="a", key_dir="<=")
+    db.update_table(source_table_name, dest_table_name, key="a", key_dir="<=")
     query_result = db.query("select * from test_table_2")
-    print(query_result)
+    assert df_expected.equals(query_result)
 
 # UPDATE TABLE STRICTLY DESCENDING test
 def test_update_table_strictly_descending(db_context):
-    df_dest = pandas.DataFrame({'a': [5, 8], 'b':[None, None], 'c':[None, None]})
-    df_source = pandas.DataFrame({'a': [1, 2, 4, 6], 'b': [2, 5, 8, 9], 'c': [3, 6, 7, 9]})
+    df_dest = pandas.DataFrame({'a': [3, 2], 'b':[4, 3], 'c':[5, 4]})
+    df_source = pandas.DataFrame({'a': [3, 2, 1, 0], 'b': [4, 3, 2, 1], 'c': [5, 4, 3, 2]})
+    df_expected = df_source
     source_table_name = "test_table_1"
     dest_table_name = "test_table_2"
     db.create_table(source_table_name, df_source)
     db.create_table(dest_table_name, df_dest)
-    db.update_table(source_table_name, dest_table_name=source_table_name, key="a", key_dir="<")
+    db.update_table(source_table_name, dest_table_name, key="a", key_dir="<")
     query_result = db.query("select * from test_table_2")
-    print(query_result)    
+    assert df_expected.equals(query_result)    
 
 # QUERY CHUNKED test
-def test_query_chunked(db_context):
-    db.create_table("test_table", df)
+# Single chunk
+def test_query_chunked_single_chunk(db_context):
+    # df_to_chunk = pandas.DataFrame({'a':[1, 4, 2], 'b':[2, 5, 9], 'c':[3, 6, 7], 'd':[3, 6, 7], 'e':[3, 6, 7], 'f':[3, 6, 7], 'g':[3, 6, 7], 'h':[3, 6, 7], 'i':[3, 6, 7], 'j':[3, 6, 7]})
+    df_to_chunk = pandas.DataFrame({'a':[1, 4, 2], 'b':[2, 5, 9], 'c':[3, 6, 7], 'd':[3, 6, 7]})
+    df_expected = df_to_chunk
+    #df_to_chunk = pandas.DataFrame({'a':[1, 4], 'b':[2, 5], 'c':[3, 6], 'd':[3, 6]})
+    db.create_table("test_table", df_to_chunk)
     query_result = db.query_chunked("select * from test_table")
-    # print(type(query_result))
+    query_result_from_generator = next(query_result)
+    assert df_expected.equals(query_result_from_generator)
+
+# 2 chunks
+def test_query_chunked_2_as_chunksize(db_context):
+    # df_to_chunk = pandas.DataFrame({'a':[1, 4, 2], 'b':[2, 5, 9], 'c':[3, 6, 7], 'd':[3, 6, 7], 'e':[3, 6, 7], 'f':[3, 6, 7], 'g':[3, 6, 7], 'h':[3, 6, 7], 'i':[3, 6, 7], 'j':[3, 6, 7]})
+    df_to_chunk = pandas.DataFrame({'a':[1, 4, 2], 'b':[2, 5, 9], 'c':[3, 6, 7], 'd':[3, 6, 7]})
+    df_expected = pandas.DataFrame({'a':[1, 4], 'b':[2, 5], 'c':[3, 6], 'd':[3, 6]})
+    #df_to_chunk = pandas.DataFrame({'a':[1, 4], 'b':[2, 5], 'c':[3, 6], 'd':[3, 6]})
+    db.create_table("test_table", df_to_chunk)
+    query_result = db.query_chunked("select * from test_table", chunksize=2)
+    query_result_from_generator = next(query_result)
+    assert df_expected.equals(query_result_from_generator)
+    # query_result_df = pandas.DataFrame(query_result_from_generator)
+    # assert df_to_chunk.equals(query_result_df)
+
+# 3 chunks
+def test_query_chunked_3_as_chunksize(db_context):
+    # df_to_chunk = pandas.DataFrame({'a':[1, 4, 2], 'b':[2, 5, 9], 'c':[3, 6, 7], 'd':[3, 6, 7], 'e':[3, 6, 7], 'f':[3, 6, 7], 'g':[3, 6, 7], 'h':[3, 6, 7], 'i':[3, 6, 7], 'j':[3, 6, 7]})
+    df_to_chunk = pandas.DataFrame({'a':[1, 4, 2, 7, 8, 0], 'b':[2, 5, 9, 4, 2, 3], 'c':[3, 6, 7, 5, 7, 7], 'd':[3, 8, 8, 3, 6, 7]})
+    df_expected = pandas.DataFrame({'a':[1, 4, 2], 'b':[2, 5, 9], 'c':[3, 6, 7], 'd':[3, 8, 8]})
+    #df_to_chunk = pandas.DataFrame({'a':[1, 4], 'b':[2, 5], 'c':[3, 6], 'd':[3, 6]})
+    db.create_table("test_table", df_to_chunk)
+    query_result = db.query_chunked("select * from test_table", chunksize=3)
+    query_result_from_generator = next(query_result)
+    # query_result_df = pandas.DataFrame(query_result_from_generator)
+    assert df_expected.equals(query_result_from_generator)
 
 
 # QUERY GROUP test
 def test_query_group(db_context):
-    db.create_table("test_table", df)
-    query_result = db.query_group("select * from test_table")
-    print(type(query_result))
+    df_to_chunk = pandas.DataFrame({'a':[1, 4, 2], 'b':[2, 5, 7], 'c':[3, 3, 3], 'd':[3, 6, 7]})
+    # df_expected = 
+    db.create_table("test_table", df_to_chunk)
+    query_result = db.query_group("select * from test_table", by=["b"])
+    query_result_from_generator = next(query_result)
+    # assert df_expected.equals(query_result_from_generator)
 
 # BEGIN test
 def test_begin(db_context):
@@ -206,21 +234,21 @@ def test_begin(db_context):
             source_table_name = "test_table_1"
             dest_table_name = "test_table_2"
             query_result_to_check_rollback = conn.query("select name from sqlite_schema where type='table'")
-            print(f"Tables before create:\n{query_result_to_check_rollback}")
+            # print(f"Tables before create:\n{query_result_to_check_rollback}")
             conn.create_table(source_table_name, df_source)
             query_result_to_check_rollback = conn.query("select name from sqlite_schema where type='table'")
             query_result_to_check_rollback2 = conn.query("select * from test_table_1")
-            print(f"Table content between both create:\n{query_result_to_check_rollback2}")
-            print(f"Tables between both create:\n{query_result_to_check_rollback}")
+            # print(f"Table content between both create:\n{query_result_to_check_rollback2}")
+            # print(f"Tables between both create:\n{query_result_to_check_rollback}")
             conn.create_table(dest_table_name, df_source)
             query_result_to_check_rollback = conn.query("select name from sqlite_schema where type='table'")
-            print(f"Tables after create:\n{query_result_to_check_rollback}")
+            # print(f"Tables after create:\n{query_result_to_check_rollback}")
             conn.update_table(source_table_name, dest_table_name, key="a", key_dir="<")   
     except:
         query_result_to_check_rollback = db.query("select name from sqlite_schema where type='table'")
-        print(f"Tables when except:\n{query_result_to_check_rollback}")
+        # print(f"Tables when except:\n{query_result_to_check_rollback}")
         query_result_to_check_rollback2 = db.query("select * from test_table_1")
-        print(f"Table content when except:\n{query_result_to_check_rollback2}")
+        # print(f"Table content when except:\n{query_result_to_check_rollback2}")
         
 
 
