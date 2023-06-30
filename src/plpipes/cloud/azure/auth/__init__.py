e9723c391617861ed89521f9a72522bff2c5a6b2
Salvador Fandiño <salvador.fandino@predictland.com>
2023-06-30 10:30:36 +0200

    Add support for multiple authentication backends for Azure

---
+++ src/plpipes/cloud/azure/auth/__init__.py
@@ -0,0 +1,20 @@
+from plpipes.config import cfg
+
+import plpipes.plugin
+
+_backend_class_registry = plpipes.plugin.Registry("azure_auth_backend",
+                                                  "plpipes.cloud.azure.auth.plugin")
+
+_registry = {}
+
+def credentials(account_name):
+    if account_name not in _registry:
+        _registry[account_name] = _init_backend(account_name)
+    return _registry[account_name].credentials()
+
+def _init_backend(account_name):
+    acfg = cfg.cd("cloud.azure.auth").cd(account_name)
+
+    backend_name = acfg.get("type", "interactive_browser")
+    backend_class = _backend_class_registry.lookup(backend_name)
+    return backend_class(account_name, acfg)
