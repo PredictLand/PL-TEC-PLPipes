1e60c44cd7d4b35a6b084e291c8a894feed22ddc
Salvador Fandiño <salvador.fandino@predictland.com>
2023-08-21 18:03:52 +0200

    Increase download timeout for us_eia net client, which is usally slow

--- src/plpipes/net/client/us_eia.py
+++ src/plpipes/net/client/us_eia.py
@@ -5,6 +5,7 @@ from plpipes.config import cfg
 
 def petroleum_prices():
     r = httpx.get("https://api.eia.gov/v2/petroleum/pri/spt/data/?frequency=weekly&data[0]=value&facets[product][]=EPCBRENT&facets[product][]=EPCWTI&sort[0][column]=period&sort[0][direction]=desc",
+                  timeout=120,
                   params = {'api_key': cfg['net.client.eia.api_key']})
     df = pd.DataFrame(r.json()['response']['data'])
     df.columns = df.columns.str.replace('-', '_')
