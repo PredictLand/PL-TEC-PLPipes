eb8e87d6db326ca1d2531bab4ba7cf2caa8f1f28
Salvador Fandino <salvador.fandino@predictland.com>
2024-10-28 20:25:37 +0100

    Add pwd/uid authentication to AzureSQL driver

--- src/plpipes/database/driver/azure_sql.py
+++ src/plpipes/database/driver/azure_sql.py
@@ -11,17 +11,31 @@ class AzureSQLDriver(ODBCDriver):
         database = drv_cfg['database']
         low_level_proto = drv_cfg.setdefault('low_level_proto', 'tcp')
         port = drv_cfg.setdefault('port', 1433)
-        cs = f"Driver={driver};Server={server};Database={database}"
+        cs = { 'Driver': driver,
+               'Server': server,
+               'Database': database }
+        # cs = f"Driver={driver};Server={server};Database={database}"
+        connect_args = {}
+
+
+        if (((pwd := drv_cfg.get('password')) is not None) and
+            ((uid := drv_cfg.get('uid')) is not None)):
+            cs['Pwd'] = pwd
+            cs['UID'] = uid
+            
+        elif (cred_account_name := drv_cfg.get('credentials')) is not None:
+            import plpipes.cloud.azure.auth
+            credential = plpipes.cloud.azure.auth.credentials(creds_account_name)
+
+            token = credential.get_token('https://database.windows.net').token.encode("utf-16-le")
+            token_struct = struct.pack(f"<I{len(token)}s", len(token), token)
+
+        else:
+            raise ValueError(f"Authentication data missing from configuration for database instance {name}")
+
+
+        cs = ";".join(f"{k}={v}" for k, v in cs.items())
         drv_cfg.setdefault('connection_string', cs)
         drv_cfg.setdefault('sql_alchemy_driver', 'mssql+pyodbc')
 
-        
-        import plpipes.cloud.azure.auth
-        creds_account_name = drv_cfg['credentials']
-        credential = plpipes.cloud.azure.auth.credentials(creds_account_name)
-
-        token = credential.get_token('https://database.windows.net').token.encode("utf-16-le")
-        token_struct = struct.pack(f"<I{len(token)}s", len(token), token)
-
-        super().__init__(name, drv_cfg, connect_args={'attrs_before': {_SQL_COPT_SS_ACCESS_TOKEN: token_struct}})
-
+        super().__init__(name, drv_cfg, connect_args=connect_args)
